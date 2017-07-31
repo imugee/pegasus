fe12b102b49369a1a519522042b9a786118e10b0
Seongbin Park <0a777h@gmail.com>
2017-07-31 11:13:01 +0900

    kpegasus:: engine 정보에 process 정보 추가

--- kpegasus/method.cpp
+++ kpegasus/method.cpp
@@ -14,28 +14,30 @@ class EXT_CLASS : public ExtExtension
 public:
 	EXT_COMMAND_METHOD(kaddress);
 };
-
-// EXT_DECLARE_GLOBALS must be used to instantiate
-// the framework's assumed globals.
 EXT_DECLARE_GLOBALS();
 
 EXT_COMMAND(kaddress,
 	"",
 	"{pid;ed,o;pid;;}")
 {
+	// unsigned long long pid = GetArgU64("pid", FALSE);
 	std::shared_ptr<engine::linker> windbg_linker;
 	if (!engine::create<windbg_engine_linker>(windbg_linker))
 		return;
 
-	if (g_Ext->IsKernelMode())
-	{
-		unsigned long long pid = GetArgU64("pid", FALSE);
-		windbg_linker->select_process(pid);
-		///
-		unsigned long long vad_size = 0;
-		windbg_process::vad_node_ptr vad = (windbg_process::vad_node_ptr)windbg_linker->get_vad_node(&vad_size);
-	}
+	windbg_process process_table[1024];
+	size_t process_count = 0;
+	windbg_linker->get_process_table(process_table, 1024, &process_count);
 
-	//if (windbg_linker->virtual_query(0, nullptr, 0))
-	//	;
+	for (size_t i = 0; i < process_count; ++i)
+	{
+		std::list<windbg_process::vad_node> vad_list = process_table[i].get_vad_list();
+		std::list<windbg_process::vad_node>::iterator vad_node = vad_list.begin();
+
+		dprintf("eprocess=%0*I64x pid=%d(0x%x)\n", 16, process_table[i].get_eprocess(), process_table[i].get_pid(), process_table[i].get_pid());
+		for (vad_node; vad_node != vad_list.end(); ++vad_node)
+		{
+			dprintf("	%0*I64x - %0*I64x\n", 16, vad_node->start, 16, vad_node->end);
+		}
+	}
 }
