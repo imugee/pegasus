7d0f2058436c304e07f227d538ebc05caf112aef
Seongbin Park <0a777h@gmail.com>
2017-08-03 11:51:57 +0900

    Update emulator.h

--- kpegasus/emulator.h
+++ kpegasus/emulator.h
@@ -1,14 +1,14 @@
 #ifndef __DEFINE_PEGASUS_EMULATOR_HEADER
 #define __DEFINE_PEGASUS_EMULATOR_HEADER
 
-class emulation_debugger : public engine::debugger
+class r3_emulation_debugger : public engine::debugger
 {
 private:
 	//std::shared_ptr<engine::linker> windbg_linker_;
 	windbg_engine_linker windbg_linker_;
 
 public:
-	emulation_debugger();
+	r3_emulation_debugger();
 };
 
 #endif // !__DEFINE_PEGASUS_EMULATOR_HEADER
